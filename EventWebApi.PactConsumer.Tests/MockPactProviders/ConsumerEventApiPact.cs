33d8acfd66877cae55830a1828d6d3749838e6a9
Vijay Vepakomma <vijay.vepakomma@philips.com>
2017-01-16 08:41:51 -0500

    Added ConsumerEventApiPact

--- EventWebApi.PactConsumer.Tests/MockPactProviders/ConsumerEventApiPact.cs
+++ EventWebApi.PactConsumer.Tests/MockPactProviders/ConsumerEventApiPact.cs
@@ -4,26 +4,13 @@ using PactNet.Mocks.MockHttpService;
 
 namespace EventWebApi.PactConsumer.Tests.MockPactProviders
 {
-    public class ConsumerEventApiPact : IDisposable
+    public class ConsumerEventApiPact : PactBase
     {
-        public IPactBuilder PactBuilder { get; private set; }
-        public IMockProviderService MockProviderService { get; private set; }
-
-        public int MockServerPort { get; private set; }
-        public string MockProviderServiceBaseUri => $"http://localhost:{MockServerPort}";
-
-        public ConsumerEventApiPact()
+        
+        public ConsumerEventApiPact() : base("Consumer", "Event API", 1234)
         {
-            PactBuilder = new PactBuilder()
-                .ServiceConsumer("Consumer")
-                .HasPactWith("Event API");
-            MockServerPort = 1234;
-            MockProviderService = PactBuilder.MockService(MockServerPort);
+            
         }
-
-        public void Dispose()
-        {
-            PactBuilder.Build();
-        }
+        
     }
 }
---
+++ EventWebApi.PactConsumer.Tests/PactTests/EventsApiConsumerPactTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using EventWebApi.Client;
+using EventWebApi.PactConsumer.Tests.MockPactProviders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PactNet.Mocks.MockHttpService.Models;
+
+namespace EventWebApi.PactConsumer.Tests.PactTests
+{
+    [TestClass]
+    public class EventsApiConsumerPactTests
+    {
+        private static ConsumerEventApiPact ConsumerEventApiPact { get;  set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            ConsumerEventApiPact = new ConsumerEventApiPact();
+
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            ConsumerEventApiPact.Service.ClearInteractions();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            ConsumerEventApiPact.Build();
+        }
+
+
+        private readonly Dictionary<string,string> RequestHeaders = 
+            new Dictionary<string, string> { { "Accept", "application/json"} };
+        private readonly Dictionary<string, string> ResponseHeaders =
+            new Dictionary<string, string> { { "Content-Type", "application/json" } };
+
+        [TestMethod]
+        public void EventApi_GetEvents_NoAuthorization()
+        {
+            ConsumerEventApiPact.Service.Given("interactions exist")
+                .UponReceiving("a valid request without authorization for GetEvents")
+                .With(new ProviderServiceRequest {Method = HttpVerb.Get, Path = "/events", Headers = RequestHeaders})
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Headers = ResponseHeaders,
+                    Status = 401,
+                    Body = new {message = "Authorization has been denied for this request"}
+                });
+
+            var client = new EventsApiClient(ConsumerEventApiPact.BaseUri);
+
+            try
+            {
+                var response = client.GetAllEvents();
+            }
+            catch (HttpRequestException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("Authorization has been denied for this request"));
+            }
+
+            ConsumerEventApiPact.Service.VerifyInteractions();
+        }
+
+        [TestMethod]
+        public void EventApi_GetAllEvents_Valid()
+        {
+            var authToken = "someValidAuthToken";
+            var headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" },
+                {"Authorization", $"Bearer {authToken}" }
+            };
+
+            ConsumerEventApiPact.Service.Given("interactions exist")
+                .UponReceiving("a valid request for GetEvents")
+                .With(new ProviderServiceRequest {Method = HttpVerb.Get, Path = "/events", Headers = headers})
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = ResponseHeaders,
+                    Body = new[]
+                    {
+                        new
+                        {
+                            eventId = Guid.Parse("45D80D13-D5A2-48D7-8353-CBB4C0EAABF5"),
+                            timestamp = "2014-06-30T01:37:41.0660548",
+                            eventType = "SearchView"
+                        },
+                        new
+                        {
+                            eventId = Guid.Parse("83F9262F-28F1-4703-AB1A-8CFD9E8249C9"),
+                            timestamp = "2014-06-30T01:37:52.2618864",
+                            eventType = "DetailsView"
+                        },
+                        new
+                        {
+                            eventId = Guid.Parse("3E83A96B-2A0C-49B1-9959-26DF23F83AEB"),
+                            timestamp = "2014-06-30T01:38:00.8518952",
+                            eventType = "SearchView"
+                        }
+                    }
+                });
+
+            var client = new EventsApiClient(ConsumerEventApiPact.BaseUri, authToken);
+            var events = client.GetAllEvents();
+
+            Assert.AreEqual(3, events.Count());
+
+        }
+    }
+}
